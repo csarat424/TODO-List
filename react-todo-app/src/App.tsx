03ddc4a47576a2300970718a48e5c22a9fd971d4
csarat424 <csarat424@gmail.com>
2024-03-13 10:28:19 -0400

    Intial Commit

---
+++ react-todo-app/src/App.tsx
@@ -0,0 +1,49 @@
+// src/App.tsx
+import React, { useState } from 'react';
+import './App.css';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo }]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
